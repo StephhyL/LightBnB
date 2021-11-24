f3ba32470ee15cd90be0beec802874dbf0877d71
StephhyL <Stephanie Li>
2021-11-24 15:16:26 -0500

    inital queries code for LHL activity - LighthouseBnB SELECT

---
+++ 1_queries/01_user_login.sql
@@ -0,0 +1,10 @@
+-- get details about a single user
+
+SELECT * 
+FROM users
+WHERE email = 'tristanjacobs@gmail.com';
+
+-- COMPASS SOLUTION:
+-- SELECT id, name, email, password
+-- FROM users
+-- WHERE email = 'tristanjacobs@gmail.com';
---
+++ 1_queries/05_all_my_reservations.sql
@@ -0,0 +1,30 @@
+-- Show all reservations for a user.
+
+-- Select all columns from the reservations table, all columns from the properties table, and the average rating of the property.
+-- The reservations will be for a single user, so use 1 for the user_id.
+-- Order the results from the earliest start_date to the most recent start_date.
+-- These will end up being filtered by either "Upcoming Reservations" or "Past Reservations", so only get reservations where the end_date is in the past.
+-- Use now()::date to get today's date.
+-- Limit the results to 10.
+
+SELECT reservations.*, properties.*,
+AVG(property_reviews.rating) as average_rating
+FROM properties
+JOIN reservations ON reservations.property_id = properties.id
+JOIN property_reviews ON property_reviews.property_id = properties.id
+WHERE reservations.guest_id = 1 AND reservations.end_date < now()::date
+GROUP BY reservations.id, properties.id
+ORDER BY reservations.start_date
+LIMIT 10;
+
+
+--COMPASS SOLUTION:
+-- SELECT properties.*, reservations.*, avg(rating) as average_rating
+-- FROM reservations
+-- JOIN properties ON reservations.property_id = properties.id
+-- JOIN property_reviews ON properties.id = property_reviews.property_id
+-- WHERE reservations.guest_id = 1
+-- AND reservations.end_date < now()::date
+-- GROUP BY properties.id, reservations.id
+-- ORDER BY reservations.start_date
+-- LIMIT 10;
