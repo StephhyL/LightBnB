b222a866bf1aa3661d9cfcdc981a1516fcfe86b7
StephhyL <Stephanie Li>
2021-11-25 12:51:05 -0500

    adjust code to fetch data from a database using Node Postgres for function getAllProperties

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -176,8 +176,58 @@ exports.getAllReservations = getAllReservations;
  */
 
 const getAllProperties = (options, limit = 10) => {
+
+  const queryParams = [];
+
+  let queryString = 
+  `
+  SELECT properties.*,
+  AVG(property_reviews.rating) AS average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `
+
+  if (options.owner_id) {
+    queryParams.push(options.owner_id);
+    queryString += `WHERE owner_id = $${queryParams.length}`
+  }
+
+  if (options.city) {
+    queryParams.push(`%${options.city}%`)
+    queryString += ` WHERE city LIKE $${queryParams.length}`
+  }
+
+  if(options.minimum_price_per_night) {
+    queryParams.push(options.minimum_price_per_night * 100);
+    options.city ? queryString += ` AND` : queryString += ` WHERE`;
+    queryString += ` cost_per_night >= $${queryParams.length}`
+  }
+
+
+  if (options.maximum_price_per_night) {
+    queryParams.push(options.maximum_price_per_night * 100);
+    options.city || options.minimum_price_per_night? queryString += ` AND` : queryString += ` WHERE`;
+    queryString += ` cost_per_night <= $${queryParams.length}`
+  }
+
+  queryString += ` GROUP BY properties.id`
+
+  if (options.minimum_rating) {
+    queryParams.push(options.minimum_rating);
+    queryString += ` HAVING AVG(property_reviews.rating) >= $${queryParams.length}`
+  }
+
+  queryParams.push(limit);
+  queryString += 
+  `
+  LIMIT $${queryParams.length};
+  `
+
+  console.log("queryString---->", queryString);
+  console.log("queryParams---->", queryParams);
+
   return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
+    .query(queryString, queryParams)
     .then((result)=> result.rows)
     .catch((err) => console.log(err.message))
   // const limitedProperties = {};
