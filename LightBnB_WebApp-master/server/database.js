b657b20a8de361d5a20dd985a3116618220a5e01
StephhyL <Stephanie Li>
2021-11-25 14:47:23 -0500

    code for addProperty to add to database using Node Postgres

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -244,10 +244,106 @@ exports.getAllProperties = getAllProperties;
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+const addProperty = (property) => {
+
+  let insertNames = 'owner_id'
+  const values = [property.owner_id];
+  
+  if (property.title) {
+    insertNames += ", title"
+    values.push(property.title)
+  }
+
+  if (property.description) {
+    insertNames += ", description"
+    values.push(property.description);
+  }
+  
+  if (property.thumbnail_photo_url) {
+    insertNames += ", thumbnail_photo_url"
+    values.push(property.thumbnail_photo_url);
+  }
+
+  if (property.cover_photo_url) {
+    insertNames += ", cover_photo_url"
+    values.push(property.cover_photo_url);
+  }
+
+  if (property.cost_per_night) {
+    insertNames += ", cost_per_night"
+    values.push(property.cost_per_night);
+  }
+
+  if (property.parking_spaces) {
+    insertNames += ", parking_spaces"
+    values.push(property.parking_spaces);
+  }
+
+  if (property.number_of_bathrooms) {
+    insertNames += ", number_of_bathrooms"
+    values.push(property.number_of_bathrooms);
+  }
+
+  if (property.number_of_bedrooms) {
+    insertNames += ", number_of_bedrooms"
+    values.push(property.number_of_bedrooms);
+  } 
+
+  if (property.country) {
+    insertNames += ", country"
+    values.push(property.country);
+  } 
+
+  if (property.street) {
+    insertNames += ", street"
+    values.push(property.street);
+  } 
+
+  if (property.city) {
+    insertNames += ", city"
+    values.push(property.city);
+  } 
+
+  if (property.province) {
+    insertNames += ", province"
+    values.push(property.province);
+  } 
+
+  if (property.post_code) {
+    insertNames += ", post_code"
+    values.push(property.post_code);
+  } 
+
+  const insertValues = () => {
+    let stringValue = "";
+    for (let i=1; i <= values.length; i++) {
+      stringValue += `$${i}`;
+      (i !== values.length) ? stringValue += ", ": stringValue
+    }
+    return stringValue;
+  }
+
+  const insertString = 
+  `
+  INSERT INTO properties (${insertNames})
+  VALUES(${insertValues()})
+  RETURNING *
+  `
+
+  console.log("insertString---->", insertString);
+  console.log("value array---->", values);
+
+  return pool
+    .query (insertString, values)
+    .then(res => res.rows)
+    .catch((err) => {
+      console.error(err.message);
+      return err.message;
+    })
+
+  // const propertyId = Object.keys(properties).length + 1;
+  // property.id = propertyId;
+  // properties[propertyId] = property;
+  // return Promise.resolve(property);
 }
 exports.addProperty = addProperty;
